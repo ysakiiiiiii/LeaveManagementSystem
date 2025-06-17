491751b1cfe168983c971b10a5ca1593923fd735
@programmingst <lucky.a.acidera@gmail.com>
2025-06-17 22:40:45 +0800

    So Fast

--- LeaveManagementSystem.Web/Controllers/CategoryController.cs
+++ LeaveManagementSystem.Web/Controllers/CategoryController.cs
@@ -17,5 +17,18 @@ namespace LeaveManagementSystem.Web.Controllers
            List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
--- LeaveManagementSystem.Web/Models/Category.cs
+++ LeaveManagementSystem.Web/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace LeaveManagementSystem.Web.Models
 {
@@ -7,7 +8,9 @@ namespace LeaveManagementSystem.Web.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-        public  int  DisplayOrder { get; set; }
+        [DisplayName("Display Order")]
+        public int  DisplayOrder { get; set; }
     }
 }
