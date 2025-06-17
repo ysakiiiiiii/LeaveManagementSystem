b943def2b4da3cf644967c0f76659486ed1ab35f
@programmingst <lucky.a.acidera@gmail.com>
2025-06-17 19:25:05 +0800

    DBMIgrations

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagementSystem.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+
         }
+
+        public DbSet<Category> Categories { get; set; }
+
+
     }
 }
--- LeaveManagementSystem.Web/Models/Category.cs
+++ LeaveManagementSystem.Web/Models/Category.cs
@@ -1,6 +1,13 @@
-﻿namespace LeaveManagementSystem.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Web.Models
 {
     public class Category
     {
+
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public  int  DisplayOrder { get; set; }
     }
 }
