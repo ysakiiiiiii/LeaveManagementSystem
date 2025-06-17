30839ba91b3e1ea8b10339321fdf9317e8abaf81
@programmingst <lucky.a.acidera@gmail.com>
2025-06-17 20:24:22 +0800

    ModelBuilder

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -13,6 +13,19 @@ namespace LeaveManagementSystem.Web.Data
 
         public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name= "Bag", DisplayOrder= 1},
+                new Category { Id = 2, Name = "Cap", DisplayOrder = 2},
+                new Category { Id = 3, Name = "Accessories", DisplayOrder = 3}
+                );
+        }
+
+        
+
 
     }
 }
