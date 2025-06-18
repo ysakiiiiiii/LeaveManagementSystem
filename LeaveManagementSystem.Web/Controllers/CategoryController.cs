57e76aafdc658e2d7a35cac4f35411c25fc95ebb
@programmingst <lucky.a.acidera@gmail.com>
2025-06-18 14:24:06 +0800

    POST and GET

--- LeaveManagementSystem.Web/Controllers/CategoryController.cs
+++ LeaveManagementSystem.Web/Controllers/CategoryController.cs
@@ -69,24 +69,49 @@ namespace LeaveManagementSystem.Web.Controllers
         [HttpPost]
         public IActionResult Edit(Category obj)
         {
-            if (obj.Name == obj.DisplayOrder.ToString())
-            {
-                ModelState.AddModelError("name", "Display order must not match with the name!");
-            }
-
-            if (obj.Name.ToLower() == "test")
-            {
-                ModelState.AddModelError("", "Display order must not match with the name!");
-            }
-
             if (ModelState.IsValid)
             {
-                _db.Categories.Add(obj);
+                _db.Categories.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
             return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //Works only on primary key searching
+            Category? categoryFromDb = _db.Categories.Find(id);
+            //Category? categoryFromDb = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //Category? categoryFromDb = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+
         }
     }
 }
